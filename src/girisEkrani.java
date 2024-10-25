05a51c3293f11600c6eabb88e9dd7a2e58ed6aa5
akiferdem <akiferdemboylu@gmail.com>
2024-10-25 17:02:52 +0300

    Commit v1.2

---
+++ src/girisEkrani.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class girisEkrani extends JFrame {
+    private JPasswordField tbParola;
+    private JTextField tbKadi;
+    private JButton btnGirisYap;
+    private JButton btnKayitOl;
+
+    public girisEkrani() {
+        // Set up the JFrame properties
+        setTitle("Giriş Ekranı");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+    }
+}
